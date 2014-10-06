fabd70eb2d68f2afbc0ef3850a10d7ecf6c036c3
Getaji <t.tanaka.onion@gmail.com>
2014-10-06 22:35:03 +0900

    redrabbit-1 ツイート

--- src/main/java/com/getaji/rrt/viewmodel/MainWindowViewModel.java
+++ src/main/java/com/getaji/rrt/viewmodel/MainWindowViewModel.java
@@ -1,16 +1,21 @@
 package com.getaji.rrt.viewmodel;
 
 import com.getaji.rrt.model.MainWindowModel;
+import com.getaji.rrt.model.StaticObjects;
 import com.getaji.rrt.model.WindowStatusType;
 import com.getaji.rrt.view.MainWindowView;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import twitter4j.TwitterException;
 
 /**
  * javadoc here.
  *
  * @author Getaji
  */
+@Log4j2
 public class MainWindowViewModel {
 
     @Getter private final MainWindowModel model = new MainWindowModel();
@@ -18,6 +23,21 @@ public class MainWindowViewModel {
 
     public MainWindowViewModel(Stage stage) {
         view = new MainWindowView(stage);
+        view.addTextAreaHandler(e -> {
+            log.debug(e.getCode());
+            if (e.isControlDown() && e.getCode() == KeyCode.ENTER) {
+                try {
+                    StaticObjects.getCurrentTwitter().updateStatus(
+                            view.getInputText()
+                    );
+                    view.setInputText("");
+                    setWindowStatus(WindowStatusType.SUCCESS, "ツイートに成功:" + view.getInputText());
+                } catch (TwitterException e1) {
+                    setWindowStatus(WindowStatusType.ERROR, "ツイートに失敗:" + view.getInputText());
+                    log.error(e1);
+                }
+            }
+        });
     }
 
     public TimelineViewModel addTimeline() {
